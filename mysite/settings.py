a96f9ebbe5d6100ebf5dc1b8d5e60c98a00cb7b2
Raimundo Moraga <raimundomoraga@hotmail.com>
2021-04-29 17:47:26 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,5 +122,3 @@ STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
