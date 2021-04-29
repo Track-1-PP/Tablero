7c7be6df5d838647d54d311aaaad15950b28fc81
Raimundo Moraga <raimundomoraga@hotmail.com>
2021-04-29 16:49:29 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'iterview/static'),
 )
 
 # Quick-start development settings - unsuitable for production
