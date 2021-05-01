abe1a872080d42767e237f2a479d988d3c04bec5
Raimundo Moraga <raimundomoraga@hotmail.com>
2021-05-01 12:40:03 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,6 +100,24 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#Logs for production
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
